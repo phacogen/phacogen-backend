c993637719ac716e6257fde8264d80147a4c68f1
Uyenlun113 <thiuyen1132004@gmail.com>
2026-02-11 15:00:11 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,27 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AuthModule } from './auth/auth.module';
 import { ClinicModule } from './clinic/clinic.module';
+import { EmailModule } from './email/email.module';
+import { NotificationModule } from './notification/notification.module';
 import { OrderModule } from './order/order.module';
 import { RoleModule } from './role/role.module';
 import { SampleCollectionModule } from './sample-collection/sample-collection.module';
-import { UserModule } from './user/user.module';
-import { EmailModule } from './email/email.module';
-import { WorkScheduleModule } from './work-schedule/work-schedule.module';
-import { WorkContentModule } from './work-content/work-content.module';
-import { NotificationModule } from './notification/notification.module';
 import { SupplyModule } from './supply/supply.module';
+import { UserModule } from './user/user.module';
+import { WorkContentModule } from './work-content/work-content.module';
+import { WorkScheduleModule } from './work-schedule/work-schedule.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, 
+      isGlobal: true,
       envFilePath: '.env', // Path to .env file
     }),
     ScheduleModule.forRoot(), // Enable scheduled tasks
-    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb+srv://uyenptforimex_db_user:Ptuylht090821@cluster0.z6zymcn.mongodb.net/thumauxetnghiem?retryWrites=true&w=majority', {
-      tls: true,
-      tlsAllowInvalidCertificates: true,
-      tlsAllowInvalidHostnames: true,
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 45000,
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb+srv://uyenptforimex_db_user:Ptuylht090821@cluster0.z6zymcn.mongodb.net/tiembanh?retryWrites=true&w=majority', {
     }),
     AuthModule,
     UserModule,
